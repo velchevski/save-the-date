7eeba187e7ec6229b8af9afeca5fad45498c8953
Martin Velchevski <martin.velchevski@gmail.com>
2020-12-08 01:03:56 +0100

    - Added loading state - Animations now trigger upon readystatechange = complete - Added easter egg - Tweaked styles

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Konami
+var konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	if (konamiCode.indexOf(event.key) < 0 || event.key !== konamiCode[current]) {
+		current = 0;
+		return;
+	}
+	current++;
+
+	if (konamiCode.length === current) {
+    current = 0;
+    window.location.replace("https://www.youtube.com/watch?v=6nnwn5rXEio");
+	}
+
+};
+
+document.addEventListener('readystatechange', (event) => {
+  if (document.readyState == "complete") {
+    document.getElementById("loader").classList.add("loader-site-loaded");
+    document.getElementById("save-the-date-card").classList.add("card-appear");
+    var animatables = document.getElementsByClassName("animatable");
+    for(var i = 0; i < animatables.length; i++) {
+      animatables[i].classList.add("animate");
+      animatables[i].setAttribute("style", `animation-delay: ${i*200}ms`);
+    }
+    document.addEventListener('keydown', keyHandler, false);
+    console.log(`%c
+    =====================================================================
+    ||  Greetings adventurer! You dug deep and stumbled onto a quest!  ||
+    ||    Before you lies an obsidian stone carved with the words:     ||
+    || ---------"REMEBER THE KONAMI DAYS AND ENTER THE CODE"---------  ||
+    || -----------------------"HINT: UP, UP..." ---------------------  ||
+    =====================================================================`
+    , "font-family:monospace");
+  };
+});
